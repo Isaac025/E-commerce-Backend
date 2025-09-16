b65acec1504e319c6ef993135286a8c30a9e0587
Isaac025 <isaacolayiwola85@gmail.com>
2025-09-16 14:07:09 +0100

    updated

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -6,26 +6,27 @@ const addToCart = async (req, res) => {
     const { userId, itemId, size } = req.body;
 
     const userData = await USER.findById(userId);
-    let cartData = await userData.cartData;
+    let cartData = userData.cartData;
 
     if (cartData[itemId]) {
       if (cartData[itemId][size]) {
         cartData[itemId][size] += 1;
       } else {
-        cartData[itemId][size];
+        cartData[itemId][size] = 1;
       }
     } else {
       cartData[itemId] = {};
       cartData[itemId][size] = 1;
     }
 
-    await USER.findByIdAndUpdate(userId, { cartData });
-    res.status(201).json({ success: true, message: "Added to Cart" });
+    await USER.findByIdAndUpdate(userId, { cartData }, { new: true });
+    res.status(201).json({ success: true, message: "Added to Cart", cartData });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
 const updateCart = async (req, res) => {
   try {
     const { userId, itemId, size, quantity } = req.body;
