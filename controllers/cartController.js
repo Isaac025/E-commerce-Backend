7b699d7148f585c70dc363fc4fa354c20c07d7d6
Isaac025 <isaacolayiwola85@gmail.com>
2025-09-19 13:47:36 +0100

    updated

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -31,19 +31,12 @@ const updateCart = async (req, res) => {
   try {
     const { userId, itemId, size, quantity } = req.body;
     const userData = await USER.findById(userId);
+    let cartData = await userData.cartData;
 
-    if (!userData) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
-    }
-
-    let cartData = userData.cartData || {};
-    if (!cartData[itemId]) cartData[itemId] = {};
     cartData[itemId][size] = quantity;
 
     await USER.findByIdAndUpdate(userId, { cartData });
-    res.status(200).json({ success: true, message: "Cart Updated", cartData });
+    res.status(200).json({ success: true, message: "Cart Updated" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: error.message });
@@ -54,14 +47,7 @@ const getUserCart = async (req, res) => {
   try {
     const { userId } = req.body;
     const userData = await USER.findById(userId);
-
-    if (!userData) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
-    }
-
-    const cartData = userData.cartData || {};
+    let cartData = await userData.cartData;
 
     res.status(200).json({ success: true, cartData });
   } catch (error) {
